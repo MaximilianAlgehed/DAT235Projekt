1cdaf2723c9601f769cefdc5d70d672fbf23844c
Maximilian Algehed <m.algehed@gmail.com>
2016-10-04 21:36:08 +0200

    Starting to muck about with how to combine EDSLs

--- src/DatatypesAlacarte.hs
+++ src/DatatypesAlacarte.hs
@@ -3,7 +3,11 @@
              TypeOperators,
              MultiParamTypeClasses,
              FlexibleContexts,
-             FlexibleInstances #-}
+             FlexibleInstances,
+             OverlappingInstances #-}
+
+module DatatypesAlacarte where
+import Control.Monad
 
 -- Generalised sum types from datatypes á la carte
 data (f :+: g) a = Inl (f a) | Inr (g a)
@@ -15,20 +19,39 @@ instance (Functor f, Functor g) => Functor (f :+: g) where
 -- Fixpoints
 data Fix f = Fix (f (Fix f))
 
--- catamorphism from \muf -> a
 cata :: (Functor f) => (f a -> a) -> Fix f -> a
 cata alg (Fix f) = alg (fmap (cata alg) f)
 
+-- Free monad
+data Free f a = Pure a | Impure (f (Free f a))
+
+instance (Functor f) => Functor (Free f) where
+    fmap f (Pure a) = Pure (f a)
+    fmap f (Impure a) = Impure (fmap (fmap f) a)
+
+instance (Functor f) => Applicative (Free f) where
+    pure = return
+    (<*>) = ap
+
+instance (Functor f) => Monad (Free f) where
+    return = Pure
+
+    (Pure a) >>= f = f a
+    (Impure a) >>= f = Impure $ fmap (>>=f) a
+
 -- subtyping for higher kinds
 -- this can probably be generalised
-class a :<: b where
-    inf :: a -> b
+class f :<: g where
+    inf :: f a -> g a
 
-instance a :<: a where
+instance f :<: f where
     inf = id
 
-instance (f a) :<: ((f :+: g) a) where
+instance f :<: (f :+: g) where
     inf = Inl
 
-instance ((f a) :<: (g a)) => (f a) :<: ((h :+: g) a) where
+instance (f :<: g) => f :<: (h :+: g) where
     inf = Inr . inf
+
+inject :: (g :<: f) => g (Free f a) -> Free f a
+inject = Impure . inf
---
+++ src/ExtendEDSL.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE GADTs, TypeOperators, FlexibleContexts #-}
+import DatatypesAlacarte
+import EDSL_Compilation
+
+-- These denote our "High" group expressions
+-- We will also need some "low" group expressions
+-- in the target monad (like the Run monad)
+data Group where
+    Get          :: Int -> Group
+    Union        :: Group -> Group -> Group
+    Intersection :: Group -> Group -> Group
+
+reifyGroup :: Group -> Prog HighExp Int
+reifyGroup (Get i)       = return i
+reifyGroup (Union gl gr) = do
+                             l <- reifyGroup gl
+                             r <- reifyGroup gr
+                             printStr $ "taking union "++(show l)++", "++(show r)
+                             return (l+r)
+reifyGroup (Intersection gl gr) = do
+                             l <- reifyGroup gl
+                             r <- reifyGroup gr
+                             printStr $ "taking intersection "++(show l)++", "++(show r)
+                             return (l+r)  
+
+type Comm = Int
+
+data GetGroup  t = GetGroup Comm (Group -> t)
+data CComm t = CComm Group (Comm -> t) 
+
+instance Functor GetGroup where
+    fmap f (GetGroup i c) = GetGroup i (f . c)
+
+instance Functor CComm where
+    fmap f (CComm g c) = CComm g (f . c)
+
+getGroup :: (GetGroup :<: f) => Comm -> Free f Group
+getGroup comm = inject (GetGroup comm Pure)
+
+createComm :: (CComm :<: f) => Group -> Free f Comm
+createComm group = inject (CComm group Pure)
+
+class EDSLAlgebra f where
+    runEDSL :: f (Prog HighExp a) -> Prog HighExp a
+
+instance EDSLAlgebra GetGroup where
+    runEDSL (GetGroup i cont) = do
+                                    printStr $ "getting group "++(show i)
+                                    cont (Get i)
+
+instance EDSLAlgebra CComm where
+    runEDSL (CComm group cont) = do
+                                    c <- reifyGroup group
+                                    cont c
+
+example :: Free (GetGroup :+: CComm) Comm
+example = do
+            g <- getGroup 5
+            createComm (Union g g)
