25f4b47dc19cf4493f8d4e54b8aade3c100b7266
Maximilian Algehed <m.algehed@gmail.com>
2016-10-04 22:48:14 +0200

    This method can combine EDSLs sideways, it looks like

--- src/ExtendEDSL.hs
+++ src/ExtendEDSL.hs
@@ -1,6 +1,7 @@
-{-# LANGUAGE GADTs, TypeOperators, FlexibleContexts #-}
+{-# LANGUAGE GADTs, TypeOperators, FlexibleContexts, FlexibleInstances #-}
 import DatatypesAlacarte
 import EDSL_Compilation
+import Control.Monad
 
 -- These denote our "High" group expressions
 -- We will also need some "low" group expressions
@@ -10,6 +11,8 @@ data Group where
     Union        :: Group -> Group -> Group
     Intersection :: Group -> Group -> Group
 
+-- TODO: Rewrite this to pass a countinuation around that frees
+-- interm groups
 reifyGroup :: Group -> Prog HighExp Int
 reifyGroup (Get i)       = return i
 reifyGroup (Union gl gr) = do
@@ -25,7 +28,7 @@ reifyGroup (Intersection gl gr) = do
 
 type Comm = Int
 
-data GetGroup  t = GetGroup Comm (Group -> t)
+data GetGroup t = GetGroup Comm (Group -> t)
 data CComm t = CComm Group (Comm -> t) 
 
 instance Functor GetGroup where
@@ -54,13 +57,21 @@ instance EDSLAlgebra CComm where
                                     printStr $ "creating communicator "++(show c)
                                     cont c
 
+instance EDSLAlgebra (Prog HighExp) where
+    runEDSL = join 
+
 instance (EDSLAlgebra f, EDSLAlgebra g) => EDSLAlgebra (f :+: g) where
     runEDSL (Inl f) = runEDSL f
     runEDSL (Inr g) = runEDSL g
 
-example :: Free (GetGroup :+: CComm) Comm
+-- The next goal is obviously to get rid of the need for this ugly thing!
+free :: (Prog HighExp :<: f) => Prog HighExp a -> Free f a
+free prog = inject $ prog >>= (\x -> return $ Pure x)
+
+example :: Free (GetGroup :+: (CComm :+: (Prog HighExp))) Comm
 example = do
             g <- getGroup 1
             g1 <- getGroup 10
             g2 <- getGroup 100
+            free $ printStr "You can, with some effort, put Prog HighExp statements in here too!"
             createComm (Intersection g2 (Union g g1))
